b30f5b055f70e699c879758fb54976016450f58b
nikosajadi <na.sajadii.29@gmail.com>
2022-10-20 00:38:43 +0200

    Date attribute

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('counter-form');
 const dateEL =document.getElementById('date-picker');
+
+//set Date input Min with Today's date
+const today = new Date().toISOString().split('T')[0];
+ dateEL.setAttribute('min',today);
