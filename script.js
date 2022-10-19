6d465c9112c8d508f2e7329bae3b0b350ebf676b
nikosajadi <na.sajadii.29@gmail.com>
2022-10-19 17:50:56 +0200

    script

---
+++ script.js
@@ -0,0 +1,3 @@
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('counter-form');
+const dateEL =document.getElementById('date-picker');
