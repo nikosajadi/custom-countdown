155955114aebe05d1ff42b5de8e116d8f6abeff7
nikosajadi <na.sajadii.29@gmail.com>
2022-10-21 11:02:15 +0200

    Form Values

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('counter-form');
 const dateEL =document.getElementById('date-picker');
 
+let countdowntitle ='';
+let countdownDate = '';
+
+
+
+
 //set Date input Min with Today's date
 const today = new Date().toISOString().split('T')[0];
+// in this way you didant accsses before today
  dateEL.setAttribute('min',today);
+
+
+ //Take values from Input
+ function updateCountdown(e) {
+   //The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
+    e.preventDefault();
+    countdowntitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[0].value;
+    console.log(countdowntitle, countdownDate);
+ }
+
+
+ //event listeners
+
+ countdownForm.addEventListener('submit', updateCountdown);
