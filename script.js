e4351df8304aa64e0cc55b9b1e998a740d76bdc9
nikosajadi <na.sajadii.29@gmail.com>
2022-10-21 12:08:10 +0200

    Populate Countdown UI

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('counter-form');
 const dateEL =document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
 let countdowntitle ='';
 let countdownDate = '';
-
+let countdownvalue = Date;
 
 
 
@@ -14,6 +19,17 @@ const today = new Date().toISOString().split('T')[0];
  dateEL.setAttribute('min',today);
 
 
+//Populate countdoen / complete UI
+function updateDOM(){
+ const now = new Date().getTime();
+ const distance = countdownvalue - now;
+ console.log('distance' , distance);
+}
+
+
+
+
+
  //Take values from Input
  function updateCountdown(e) {
    //The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
@@ -21,6 +37,10 @@ const today = new Date().toISOString().split('T')[0];
     countdowntitle = e.srcElement[0].value;
     countdownDate = e.srcElement[0].value;
     console.log(countdowntitle, countdownDate);
+    // Get number of current Date, updateDOM
+    countdownvalue = new Date(countdownDate).getTime();
+    console.log ('Countdown value', countdownvalue)
+    updateDom();
  }
 
 
