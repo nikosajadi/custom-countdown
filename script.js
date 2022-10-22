071e56837b652071cbd761291dd021f441ff7510
nikosajadi <na.sajadii.29@gmail.com>
2022-10-22 13:45:02 +0200

    Populate countdoen / complete UI

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+const countdownForm = document.getElementById('countdownForm');
 const inputContainer = document.getElementById('input-container');
-const countdownForm = document.getElementById('counter-form');
-const dateEL =document.getElementById('date-picker');
+const dateEl = document.getElementById('date-picker');
 
 const countdownEl = document.getElementById('countdown');
 const countdownElTitle = document.getElementById('countdown-title');
 const countdownBtn = document.getElementById('countdown-button');
 const timeElements = document.querySelectorAll('span');
 
-let countdowntitle ='';
+let countdownTitle = '';
 let countdownDate = '';
-let countdownvalue = Date;
+let countdownValue = Date;
 
 const second = 1000;
 const minute = second * 60;
@@ -19,7 +19,7 @@ const day = hour * 24;
 //set Date input Min with Today's date
 const today = new Date().toISOString().split('T')[0];
 // in this way you didant accsses before today
- dateEL.setAttribute('min',today);
+dateEl.setAttribute('min',today);
 
 
 //Populate countdoen / complete UI
@@ -35,6 +35,12 @@ const minutes = Math.floor((distance % hour) / minute);
 const seconds = Math.floor((distance % minute) / second);
 console.log(days, hours, minutes, seconds)
 
+// Set populate Countdown
+countdownElTitle.textContent = `${countdownElTitle}`
+timeElements [0].textContent = `${days}`
+timeElements [1].textContent = `${hours}`
+timeElements [2].textContent = `${minutes}`
+timeElements [3].textContent = `${seconds}`
 
 // Hide Input
 inputContainer.hidden = true;
@@ -44,14 +50,14 @@ countdownEl.hidden = false;
 
  //Take values from Input
  function updateCountdown(e) {
-   //The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
-    e.preventDefault();
-    countdowntitle = e.srcElement[0].value;
-    countdownDate = e.srcElement[0].value;
-    console.log(countdowntitle, countdownDate);
+  //The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
+   e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
     // Get number of current Date, updateDOM
-    countdownvalue = new Date(countdownDate).getTime();
-    console.log ('Countdown value:', countdownvalue)
+    countdownValue = new Date(countdownDate).getTime();
+    console.log ('Countdown value:', countdownValue)
     updateDOM();
  }
 
